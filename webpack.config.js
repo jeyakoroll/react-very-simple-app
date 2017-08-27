eb858a7d98f29508173015c9fa542e1bc6b891ba
Jenya Korolenko <jeyakoroll@gmail.com>
2017-08-27 20:11:37 +0200

    done simple app

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	entry: './app/components/Main.js',
+	output: {
+		filename: 'public/bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			}
+		]
+	}
+}
